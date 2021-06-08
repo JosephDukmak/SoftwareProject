1da096fa682a2990001eb9104f7eb8430ec04a23
Joseph <josephdukmak@hotmail.com>
2021-06-08 16:31:31 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,406 @@
+<?php
+  require 'config/config.php';
+  $data = [];
+  
+  if(isset($_POST['search'])) {
+    // Get data from FORM
+    $keywords = $_POST['keywords'];
+    $location = $_POST['location'];
+
+    //keywords based search
+    $keyword = explode(',', $keywords);
+    $concats = "(";
+    $numItems = count($keyword);
+    $i = 0;
+    foreach ($keyword as $key => $value) {
+      # code...
+      if(++$i === $numItems){
+         $concats .= "'".$value."'";
+      }else{
+        $concats .= "'".$value."',";
+      }
+    }
+    $concats .= ")";
+  //end of keywords based search
+  
+  //location based search
+    $locations = explode(',', $location);
+    $loc = "(";
+    $numItems = count($locations);
+    $i = 0;
+    foreach ($locations as $key => $value) {
+      # code...
+      if(++$i === $numItems){
+         $loc .= "'".$value."'";
+      }else{
+        $loc .= "'".$value."',";
+      }
+    }
+    $loc .= ")";
+
+  //end of location based search
+    
+    try {
+      //foreach ($keyword as $key => $value) {
+        # code...
+
+        $stmt = $connect->prepare("SELECT * FROM room_rental_registrations_apartment WHERE country IN $concats OR country IN $loc OR state IN $concats OR state IN $loc OR city IN $concats OR city IN $loc OR address IN $concats OR address IN $loc OR rooms IN $concats OR landmark IN $concats OR landmark IN $loc OR rent IN $concats OR deposit IN $concats");
+        $stmt->execute();
+        $data2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $stmt = $connect->prepare("SELECT * FROM room_rental_registrations WHERE country IN $concats OR country IN $loc OR state IN $concats OR state IN $loc OR city IN $concats OR city IN $loc OR rooms IN $concats OR address IN $concats OR address IN $loc OR landmark IN $concats OR rent IN $concats OR deposit IN $concats");
+        $stmt->execute();
+        $data8 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $data = array_merge($data2, $data8);
+
+    }catch(PDOException $e) {
+      $errMsg = $e->getMessage();
+    }
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>App</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+
+    <!-- Custom fonts for this template -->
+    <link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/rent.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/superslides.css" rel="stylesheet">
+  </head>
+
+  <body id="page-top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="../crashin/images/logo00.png" alt="logo" width="80" height="80"/></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+           
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#search">Search</a>
+            </li>
+            
+            <?php 
+              if(empty($_SESSION['username'])){
+                echo '<li class="nav-item">';
+                  echo '<a class="nav-link" href="./auth/login.php">Login</a>';
+                echo '</li>';
+              }else{
+                echo '<li class="nav-item">';
+                 echo '<a class="nav-link" href="./auth/dashboard.php">Home</a>';
+               echo '</li>';
+              }
+            ?>
+            
+
+            <li class="nav-item">
+              <a class="nav-link" href="./auth/register.php">Register</a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header>
+        
+    </header>
+
+ <!-- Start slides -->
+ <div id="slides" class="cover-slides">
+		<ul class="slides-container">
+			<li class="text-center">
+				<img src="../crashin/images/slider-01.jpg" alt="bg1">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="m-b-20"><strong>Welcome To <br> CrashIn </strong></h1>
+							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
+							trends to see any changes in performance over time.</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="text-center">
+				<img src="../crashin/images/slider-02.jpg" alt="bg2">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="m-b-20"><strong>Welcome To <br> CrashIn</strong></h1>
+							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
+							trends to see any changes in performance over time.</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="text-center">
+				<img src="../crashin/images/slider-03.jpg" alt="bg3">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="m-b-20"><strong>Welcome To <br> CrashIn</strong></h1>
+							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
+							trends to see any changes in performance over time.</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
+						</div>
+					</div>
+				</div>
+			</li>
+		</ul>
+		<div class="slides-navigation">
+			<a href="#" class="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+			<a href="#" class="prev"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+		</div>
+	</div>
+	<!-- End slides -->
+  <!-- Start About -->
+	<div class="about-section-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<img src="imgs/bg3.png" alt="" class="img-fluid">
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
+					<div class="inner-column">
+						<h1>What is <span>CrashIn ?</span></h1>
+						<p>its home/apartment rental that keeps everything very simple </p>
+						<p>by having the ability to post rooms or booking them, and  all this without being restricted by the rules of regular hotels or rentals, this allow smoother dealing between the room owner and the renter, many things become esier by CRASHING IN Easily!</p>
+						<a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About -->
+    <!-- Start 1 -->
+	<div class="container1">
+		<div class=" img1">
+			<img src="imgs/housegain" alt="images-descriptionsomething">
+		</div>
+        <div class ="paragraph">
+        <p> <b> Hosting can help you turn your extra <span>space</span></b> </p>
+        <p> <b>into extra <span>income </span>and pursue more of what you love.</b></p>
+        </div>
+
+	</div>
+
+	<!-- End 1 -->
+
+    <!-- Start 2 -->
+	<div class="container1">
+		<div class=" img2">
+			<img src="imgs/house1" alt="images-descriptionsomething">
+		</div>
+        <div class ="paragraph1">
+       <p> <b> You can find multiple choices wether <span>luxury</span> or</b></p>
+        <p><b>you prefer an <span>economical</span> option</b></p>
+        <p><b>and save money and crash in successifuly </b></p>
+        </div>
+
+	</div>
+
+	<!-- End 2-->
+    
+
+
+	<!-- Start QT -->
+	<div class="qt-box qt-background">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 ml-auto mr-auto text-left">
+					<p class="lead ">
+						"The more simple we are, the more complete we become."
+					</p>
+					<span class="lead">Auguste Rodin</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End QT -->
+     <!-- Search -->
+    <section id="search">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Search</h2>
+            <h3 class="section-subheading text-muted">Search rooms or homes for hire.</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <form action="" method="POST" class="center" novalidate>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input class="form-control" id="keywords" name="keywords" type="text" placeholder="Key words(Ex: 1bhk,rent..)" required data-validation-required-message="Please enter keywords">
+                    <p class="help-block text-danger"></p>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input class="form-control" id="location" type="text" name="location" placeholder="Location" required data-validation-required-message="Please enter location.">
+                    <p class="help-block text-danger"></p>
+                  </div>
+                </div>         
+
+                <div class="col-md-2">
+                  <div class="form-group">
+                    <button id="" class="btn btn-success btn-md text-uppercase" name="search" value="search" type="submit">Search</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+
+            <?php
+              if(isset($errMsg)){
+                echo '<div style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</div>';
+              }
+              if(count($data) !== 0){
+                echo "<h2 class='text-center'>List of Apartment Details</h2>";
+              }else{
+                //echo "<h2 class='text-center' style='color:red;'>Try Some other keywords</h2>";
+              }
+            ?>        
+            <?php 
+                foreach ($data as $key => $value) {           
+                  echo '<div class="card card-inverse card-info mb-3" style="padding:1%;">          
+                        <div class="card-block">';
+                          // echo '<a class="btn btn-warning float-right" href="update.php?id='.$value['id'].'&act=';if(isset($value['ap_number_of_plats'])){ echo "ap"; }else{ echo "indi"; } echo '">Edit</a>';
+                         echo   '<div class="row">
+                            <div class="col-4">
+                            <h4 class="text-center">Owner Details</h4>';
+                              echo '<p><b>Owner Name: </b>'.$value['fullname'].'</p>';
+                              echo '<p><b>Mobile Number: </b>'.$value['mobile'].'</p>';
+                              echo '<p><b>Alternate Number: </b>'.$value['alternat_mobile'].'</p>';
+                              echo '<p><b>Email: </b>'.$value['email'].'</p>';
+                              echo '<p><b>Country: </b>'.$value['country'].'</p><p><b> State: </b>'.$value['state'].'</p><p><b> City: </b>'.$value['city'].'</p>';
+                              if ($value['image'] !== 'uploads/') {
+                                # code...
+                                echo '<img src="app/'.$value['image'].'" width="100">';
+                              }
+
+                          echo '</div>
+                            <div class="col-5">
+                            <h4 class="text-center">Room Details</h4>';
+                              // echo '<p><b>Country: </b>'.$value['country'].'<b> State: </b>'.$value['state'].'<b> City: </b>'.$value['city'].'</p>';
+                              echo '<p><b>Plot Number: </b>'.$value['plot_number'].'</p>';
+
+                              if(isset($value['sale'])){
+                                echo '<p><b>Sale: </b>'.$value['sale'].'</p>';
+                              } 
+                              
+                                if(isset($value['apartment_name']))                         
+                                  echo '<div class="alert alert-success" role="alert"><p><b>Apartment Name: </b>'.$value['apartment_name'].'</p></div>';
+
+                                if(isset($value['ap_number_of_plats']))
+                                  echo '<div class="alert alert-success" role="alert"><p><b>Plat Number: </b>'.$value['ap_number_of_plats'].'</p></div>';
+
+                              echo '<p><b>Available Rooms: </b>'.$value['rooms'].'</p>';
+                              echo '<p><b>Address: </b>'.$value['address'].'</p><p><b> Landmark: </b>'.$value['landmark'].'</p>';
+                          echo '</div>
+                            <div class="col-3">
+                            <h4>Other Details</h4>';
+                            echo '<p><b>Accommodation: </b>'.$value['accommodation'].'</p>';
+                            echo '<p><b>Description: </b>'.$value['description'].'</p>';
+                              if($value['vacant'] == 0){ 
+                                echo '<div class="alert alert-danger" role="alert"><p><b>Occupied</b></p></div>';
+                              }else{
+                                echo '<div class="alert alert-success" role="alert"><p><b>Vacant</b></p></div>';
+                              } 
+                            echo '</div>
+                          </div>              
+                         </div>
+                      </div>';
+                }
+              ?>              
+          </div>
+        </div>
+      </div>
+      <br><br><br><br><br><br>
+    </section>    
+
+    <!-- Footer -->
+    <footer style="background-color: #ccc;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <span class="copyright">Copyright &copy; Crash In 2021</span>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline social-buttons">
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+   
+    <!-- Bootstrap core JavaScript -->
+    <script src="assets/plugins/jquery/jquery.min.js"></script>
+    <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="assets/plugins/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Contact form JavaScript -->
+    <script src="assets/js/jqBootstrapValidation.js"></script>
+    <script src="assets/js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="assets/js/rent.js"></script>
+
+    <!-- ALL JS FILES -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="js/jquery.superslides.min.js"></script>
+	<script src="js/images-loded.min.js"></script>
+	<script src="js/isotope.min.js"></script>
+	
+	<script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
+
+  </body>
+</html>
